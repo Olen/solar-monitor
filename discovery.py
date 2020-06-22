b90986b2eedd76e10ad58d2e6ca532cfa969d1ef
Ola Thoresen <ola@nytt.no>
2020-06-22 20:37:37 +0200

    Deleting old files

--- discovery.py
+++
@@ -1,9 +0,0 @@
-import blegatt
-
-class AnyDeviceManager(blegatt.DeviceManager):
-    def device_discovered(self, device):
-        print("[%s] Discovered, alias = %s" % (device.mac_address, device.alias()))
-
-manager = AnyDeviceManager(adapter_name='hci0')
-manager.start_discovery()
-manager.run()
