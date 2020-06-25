b18f3cf52909290e3b2102347c07fd3f9f2fe81a
Ola Thoresen <ola@nytt.no>
2020-06-25 14:11:46 +0200

    Adding vivtronutil

---
+++ victronutil.py
@@ -0,0 +1,19 @@
+import logging                                                     
+
+
+class VictronUtil():
+    def __init__(self, device_type, power_device):                  
+        self.DeviceType = device_type     
+        self.PowerDevice = power_device                  
+
+    def notificationData(self):
+        pass
+
+    def pollData(self):
+        pass
+
+    def ackData(self):
+        pass
+
+    def validate(self):
+        pass
