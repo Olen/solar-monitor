b0c9699e3a9b8ef2b261869e53d4ea21c0300aaf
Ola Thoresen <ola@nytt.no>
2020-06-29 15:01:34 +0200

    Cleanup old files

--- blegatt/__init__.py
+++
@@ -1,9 +0,0 @@
-from __future__ import  absolute_import
-import sys
-sys.path.append('./')
-from . import gatt
-__all__ = ["blegatt"]
-
-from .gatt import DeviceManager, Device, Service, Characteristic
-
-# from .blegatt import DeviceManager, Device, Service, Characteristic
